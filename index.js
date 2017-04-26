92d3b4b469186c64ce14f9269f017490b197b74a
Leonardo Soares <leonardo.nightcrawler@gmail.com>
2017-04-26 20:02:48 -0300

    Finished all routes, the application prepared for front-end development

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const router = require('./routes/api');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const router = require('./routes/api');
 
 // set up express app
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', router);
+
+// error handling middleware
+app.use(function (err, req, res, next) {
+   res.status(422).send({error: err.message})
+});
+
+app.use(express.static('public'));
+
+// connect to the mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
 
 // def port
 const PORT = process.env.port || 4000;
--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,67 @@
 const express = require('express');
 const router = express.Router();
-
-//Temp: list of ninjas
-var ninjas = [{id: 2, name: 'Leonardo', age: 29}, {id: 1, name: 'Maria', age: 18}];
+const Ninja = require('../models/ninja');
 
 // get list of all ninjas from the db
-router.get('/ninjas', function (req, res) {
-    res.send({type: 'GET'})
+router.get('/ninjas', function (req, res, next) {
+    if(Object.keys(req.query).length === 0) {
+        Ninja.find({}, function (err, ninjas) {
+            res.json(ninjas);
+        });
+    }
+    Ninja.geoNear(
+        {type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+        {maxDistance: 100000, spherical: true},
+        function (err, ninjas) {
+            if(err) {
+                res.status(422).send({error: err.message});
+            } else {
+                res.json(ninjas);
+            }
+        }
+    );
 });
 
 // get a record of ninja from db
-router.get('/ninjas/:id', function (req, res) {
-   ninjas.forEach(function (ninja) {
-        if(ninja.id == req.params.id) {
-            res.json(ninja);
-            return;
+router.get('/ninjas/:id', function (req, res, next) {
+    Ninja.findOne({_id: req.params.id}, function (err, ninja) {
+        if(err) {
+            res.status(422).send({error: err.message});
+        } else {
+            res.send(ninja);
         }
-   });
-   res.status(404).end();
+    });
 });
 
 // add new ninja to the db
-router.post('/ninjas', function (req, res) {
-    console.log(req.body);
-    res.send({type: 'POST'})
+router.post('/ninjas', function (req, res, next) {
+    Ninja.create(req.body).then(function (ninja) {
+        res.send(ninja);
+    }).catch(next);
 });
 
 // update a ninja in db
-router.put('/ninjas/:id', function (req, res) {
-    res.send({type: 'PUT'})
+router.put('/ninjas/:id', function (req, res, next) {
+    Ninja.findByIdAndUpdate({_id: req.params.id}, req.body, function (err, ninja) {
+        if(err) {
+            res.status(422).send({error: err.message});
+        } else {
+            Ninja.findById({_id: ninja._id.toString()}, function (err, result) {
+                res.json(result);
+            });
+        }
+    });
 });
 
 // delete a ninja from the db
-router.delete('/ninjas/:id', function (req, res) {
-    res.send({type: 'DELETE'})
+router.delete('/ninjas/:id', function (req, res, next) {
+    Ninja.findByIdAndRemove({_id: req.params.id}, function (err, ninja) {
+        if(err) {
+            res.status(422).send({error: err.message});
+        } else {
+            res.json(ninja);
+        }
+    });
 });
 
 module.exports = router
